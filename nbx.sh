7ef2f74cbcca4bf2337a503e937f343d7d2464e9
Kir Belevich <kir@belevi.ch>
2020-04-05 09:45:43 +0000

    🛠 minor tweaks

--- nbx.sh
+++ nbx.sh
@@ -3,20 +3,19 @@
 BIN_DIR='./node_modules/.bin/'
 
 if [ ! -d "$BIN_DIR" ]; then
-  echo "$BIN_DIR"' directory is not found'
+  echo "$BIN_DIR directory is not found" 1>&2
   exit 1
 fi
 
 if [ $# -eq 0 ]; then
-  echo 'One of the following binary names is required:'
-  ls "$BIN_DIR"
+  echo "One of the following binary names is required: $(ls -m $BIN_DIR)" 1>&2
   exit 1
 fi
 
-BIN_FILE="$BIN_DIR""$1"
+BIN_FILE="$BIN_DIR$1"
 
 if [ ! -h "$BIN_FILE" ]; then
-  echo "$BIN_FILE"' is not found'
+  echo "$BIN_FILE is not found"
   exit 1
 fi
 
